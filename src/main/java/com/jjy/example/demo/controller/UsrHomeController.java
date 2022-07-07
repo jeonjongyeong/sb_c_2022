e3ec2fcce012d92badc075bd0de738e8f482d608
jkkjkhh <jkkjkhh@gmail.com>
2022-07-07 13:35:41 +0900

    UsrArticleController의 서비스 로직을 ArticleService 로 옮김

--- src/main/java/com/jjy/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/jjy/example/demo/controller/UsrHomeController.java
@@ -14,93 +14,14 @@ import com.jjy.example.demo.vo.Article;
 @Controller
 public class UsrHomeController {
 	
-	@RequestMapping("/usr/home/getString")
+	
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	
-	public String getString() {
-		return "HI";
-	}
-	
-	@RequestMapping("/usr/home/getInt")
-	@ResponseBody
-	
-	public int getInt() {
-		return 10;
-	}
-	
-	@RequestMapping("/usr/home/getFloat")
-	@ResponseBody
-	
-	public float getFloat() {
-		return 10.5f;
-	}
-	
-	
-	@RequestMapping("/usr/home/getDouble")
-	@ResponseBody
-	
-	public double getDouble() {
-		return 10.5;
-	}
-	
-	@RequestMapping("/usr/home/getBoolean")
-	@ResponseBody
-	
-	public boolean getBoolean() {
-		return true;
-	}
-	
-	@RequestMapping("/usr/home/getCharacter")
-	@ResponseBody
-	
-	public char getCharacter() {
-		return 'c';
-	}
-	
-	@RequestMapping("/usr/home/getList")
-	@ResponseBody
-	
-	public List<String> getList() {
-		List<String> list = new ArrayList<>();
-		list.add("철수");
-		list.add("종영");
-		return list;
-	}
-	
-	
-	@RequestMapping("/usr/home/getMap")
-	@ResponseBody
-	
-	public Map<String, Object> getMap() {
-		Map<String, Object> map = new HashMap<>();
-		map.put("철수나이",22);
-		map.put("영희나이",22);
+	public String getStarting() {
+
 		
-		return map;
-	}
-	
-	
-	@RequestMapping("/usr/home/getArticle")
-	@ResponseBody
-	
-	public Article getArticle() {
-		Article article = new Article(1, "제목", "내용");
-		
-		return article;
-	}
-	
-	@RequestMapping("/usr/home/getArticles")
-	@ResponseBody
-	
-	public List<Article> getArticles() {
-		Article article1 = new Article(1, "제목1", "내용1");
-		Article article2 = new Article(2, "제목2", "내용2");
-		
-		List<Article> list = new ArrayList<>();
-		list.add(article1);
-		list.add(article2);
-		
-		return list;
+		return "하이하이";
 	}
 
 }
