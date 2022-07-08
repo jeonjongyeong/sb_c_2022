e674523538e0a2890fd5b06cd83626b117520a3a
jkkjkhh <jkkjkhh@gmail.com>
2022-07-08 10:20:04 +0900

    SQL을 xml 파일로 옮김

--- src/main/java/com/jjy/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/jjy/example/demo/repository/ArticleRepository.java
@@ -14,22 +14,22 @@ import com.jjy.example.demo.vo.Article;
 @Mapper
 public interface ArticleRepository {
 		
-	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+	
 	public void writeArticle(@Param("title") String title,@Param("body") String body);
 	
-	@Select("SELECT * FROM article WHERE id = #{id}")
+	
 	public Article getArticle(@Param("id") int id);
 	
-	@Delete("DELETE FROM article WHERE id = #{id}")
+	
 	public void deleteArticle(@Param("id") int id);
 	
-	@Update("UPDATE article Set title = #{title}, `body` = #{body} WHERE id = #{id}")
+	
 	public void ModifyArticle(@Param("id") int id,@Param("title") String title,@Param("body") String body);
 
-	@Select("SELECT * FROM article ORDER BY id DESC")
+	
 	public List<Article> getArticles();
 
-	@Select("SELECT LAST_INSERT_ID()")
+	
 	public int getLastInsertId();
 
 	}
